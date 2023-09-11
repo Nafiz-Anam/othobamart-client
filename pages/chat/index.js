59ca86903d9ea0d6c5481ef7181a2b4f8c3f7271
Nafiz Anam <nafiz.anam002@gmail.com>
2023-09-11 09:08:20 +0600

    Update index.js

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -24,7 +24,7 @@ export default function Messenger() {
     const scrollRef = useRef();
 
     useEffect(() => {
-        socket.current = io("https://othobamart-chat-api.herokuapp.com/");
+        socket.current = io("https://othoba-mart-chat.onrender.com/");
         socket.current.on("getMessage", (data) => {
             setArrivalMessage({
                 sender: data.senderId,
